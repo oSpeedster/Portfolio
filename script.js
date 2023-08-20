910302e56705a25b51f7ce845482d4cbacb0bbe1
André Murilo <andremurilo00@gmail.com>
2023-08-20 12:06:38 -0300

    Commit 3

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 
 
+
+
+function fotosPortfolio() {
+    let itemsFotos = document.getElementById('items-fotos')
+    let pagina = document.querySelector('html')
+    let fundoopac = document.querySelector('.imagemfundo-opac')
+    let galeriaItem01 = document.getElementById('galeria-items01')
+    
+    itemsFotos.style.display = 'flex'
+    pagina.style.overflow = 'hidden'
+    fundoopac.style.display = 'none'
+
+    galeriaItem01.className = 'galeria-items-'
+    
+
+
+}
+
+function fecharFotos() {
+    let itemsFotos = document.getElementById('items-fotos')
+    let pagina = document.querySelector('html')
+    let fundoopac = document.querySelector('.imagemfundo-opac')
+    let galeriaItem01 = document.getElementById('galeria-items01')
+
+    itemsFotos.style.display = 'none'
+    pagina.style.overflow = 'auto'
+    fundoopac.style.display = 'grid'
+    galeriaItem01.className = 'galeria-items'
+
+}
+
